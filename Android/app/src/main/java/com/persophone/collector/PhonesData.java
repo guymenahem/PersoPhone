45f8559cd07d74b4414b493b95fc3ccbe32850bc
Michael Goldenberg <Michael Goldenberg>
2017-08-19 14:07:41 +0300

    Request Handler and phones data

---
+++ Android/app/src/main/java/com/persophone/collector/PhonesData.java
@@ -0,0 +1,62 @@
+package com.persophone.collector;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.ImageLoader;
+import com.android.volley.toolbox.JsonArrayRequest;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+/**
+ * Created by Michael Goldenberg on 18/08/2017.
+ */
+
+public class PhonesData {
+
+    public PhonesData(){
+        int deb = 2;
+    }
+
+    public void GetPhonesData(final Response.Listener<JSONArray> callback){
+
+        JsonArrayRequest jsObjRequest = new JsonArrayRequest
+                (Request.Method.GET, RequestHandler.URL_APP_SERVER + "/phonesList", null, callback, new Response.ErrorListener() {
+
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        // TODO Auto-generated method stub
+                        int m = 2;
+                        int m2 = 2;
+                    }
+                });
+
+        // Access the RequestQueue through your singleton class.
+        RequestHandler.getInstance(null).addToRequestQueue(jsObjRequest);
+    }
+
+    public void GetPhonesImage(String url, final ImageLoader.ImageListener listener){
+
+        RequestHandler.getInstance(null).getImageLoader().get(url, listener);
+
+    }
+
+    public void GetRecomendedPhones(String url, final Response.Listener<JSONArray> callback){
+
+        JsonArrayRequest jsObjRequest = new JsonArrayRequest
+                (Request.Method.GET, RequestHandler.URL_APP_SERVER + "/recommendedPhones", null, callback, new Response.ErrorListener() {
+
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        // TODO Auto-generated method stub
+                        int m = 2;
+                        int m2 = 2;
+                    }
+                });
+
+        // Access the RequestQueue through your singleton class.
+        RequestHandler.getInstance(null).addToRequestQueue(jsObjRequest);
+    }
+}

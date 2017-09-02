1f0424541c5d4690716c44b0d2544ffa107e5bdd
Michael Goldenberg <Michael Goldenberg>
2017-09-02 20:13:08 +0300

    Simulate Algorithem

---
+++ AdminWebsite/WebSites/PhonesManager/SimulateAlgo.aspx
@@ -0,0 +1,40 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/MasterPage.master" AutoEventWireup="true" CodeFile="SimulateAlgo.aspx.cs" Inherits="SimulateAlgo" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="title" Runat="Server">
+    Home
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="head" Runat="Server">
+</asp:Content>
+<asp:Content ID="Content3" ContentPlaceHolderID="contentBody" Runat="Server">    
+        <asp:Label ID="Label1" runat="server" Text="Enter User Id:"></asp:Label>
+        <asp:TextBox ID="txtUserID" runat="server"></asp:TextBox>
+        <br />
+        <br />
+        <br />
+        <asp:Label ID="Label2" runat="server" Text="Battery Grade:"></asp:Label>
+        <asp:TextBox ID="txtBattery" runat="server"></asp:TextBox>
+        <br />
+        <br />
+        <br />
+        <asp:Label ID="Label3" runat="server" Text="CPU Grade:"></asp:Label>
+        <asp:TextBox ID="txtCPU" runat="server"></asp:TextBox>
+        <br />
+        <br />
+        <br />
+        <asp:Label ID="Label4" runat="server" Text="RAM Grade:"></asp:Label>
+        <asp:TextBox ID="txtRam" runat="server"></asp:TextBox>
+        <br />
+        <br />
+        <br />
+        <asp:Label ID="Label5" runat="server" Text="Storage Grade:"></asp:Label>
+        <asp:TextBox ID="txtStorage" runat="server"></asp:TextBox>
+        <br />
+        <br />
+        <br />
+        <asp:Label ID="Label6" runat="server" Text="Camera Grade:"></asp:Label>
+        <asp:TextBox ID="txtCamera" runat="server"></asp:TextBox>
+        <br />
+        <br />
+        <br />
+        <asp:Button ID="btnSubmit" runat="server" OnClick="btnSubmit_Click" Text="Simulate" />
+        <br />
+</asp:Content>
---
+++ AdminWebsite/WebSites/PhonesManager/SimulateAlgo.aspx.cs
@@ -0,0 +1,64 @@
+﻿using Npgsql;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class SimulateAlgo : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+    string ConnectoinString = "Server=persodb.c9c4ima6hezo.eu-central-1.rds.amazonaws.com;Port=5432; Userid=postgres;Password=postgres;Database=postgres";
+    protected void btnSubmit_Click(object sender, EventArgs e)
+    {
+        try
+        {
+            int userId, battery, cpu, ram, storage, camera;
+            if (int.TryParse(txtUserID.Text, out userId) &&
+                int.TryParse(txtBattery.Text, out battery) &&
+                int.TryParse(txtCPU.Text, out cpu) &&
+                int.TryParse(txtRam.Text, out ram) &&
+                int.TryParse(txtStorage.Text, out storage) &&
+                int.TryParse(txtCamera.Text, out camera))
+            {
+                using (NpgsqlConnection connection = new NpgsqlConnection(ConnectoinString))
+                {
+                    try
+                    {
+                        connection.Open();
+                        NpgsqlCommand command = new NpgsqlCommand(
+                            @"select * from simulate_algorithem(@userid, @battery, @cpu ,@ram, @storage, @camera);",
+                            connection);
+
+                        command.Parameters.AddWithValue("@userid", NpgsqlTypes.NpgsqlDbType.Integer, userId);
+                        command.Parameters.AddWithValue("@battery", NpgsqlTypes.NpgsqlDbType.Integer, battery);                        
+                        command.Parameters.AddWithValue("@cpu", NpgsqlTypes.NpgsqlDbType.Integer, cpu);
+                        command.Parameters.AddWithValue("@ram", NpgsqlTypes.NpgsqlDbType.Integer, ram);
+                        command.Parameters.AddWithValue("@storage", NpgsqlTypes.NpgsqlDbType.Integer, storage);
+                        command.Parameters.AddWithValue("@camera", NpgsqlTypes.NpgsqlDbType.Integer, camera);
+                        
+
+                        command.ExecuteNonQuery();
+                        connection.Close();
+                    }
+                    catch (Exception ex)
+                    {
+                        Response.Write(ex.ToString());
+                    }
+                }
+            }
+            else
+            {
+                ClientScript.RegisterStartupScript(this.GetType(), "myalert", "alert('" + "not all fields valid" + "');", true);
+            }
+        }
+        catch (Exception ex)
+        {
+
+        }
+    }
+}
